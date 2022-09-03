329531a77b49d558e203547212deedcc3e4eb55e
EC2 Default User <ec2-user@ip-172-31-47-151.ec2.internal>
2022-09-03 10:20:05 +0000

    a

--- config/routes.rb
+++ config/routes.rb
@@ -27,9 +27,4 @@ Rails.application.routes.draw do
   # get 'users/followings'
   # get 'users/followers'
 
-  #devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
