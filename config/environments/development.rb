c4ee67fff62ca2fabd8ce49234d89d75196509dd
kaihotsuhikaru <minazuki0630@icloud.com>
2022-10-11 13:22:26 +0000

    chenge migration_files_datetime for user_model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << "8728f1a663584807922ea5142db5130e.vfs.cloud9.us-east-1.amazonaws.com"
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
@@ -40,7 +41,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
@@ -69,8 +70,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  
+
   config.hosts << "7a3726713a54426ca9862aa0f758102b.vfs.cloud9.us-east-1.amazonaws.com"
   config.active_job.queue_adapter = :inline
-  
+
 end
